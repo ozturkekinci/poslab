5289efee47d0ad0e5ae88775a854766e84e8c657
Öztürk Ekinci <ozturkekinci87@gmail.com>
2019-10-26 16:46:46 +0300

    First Push

---
+++ application/language/turkish/turkish_lang.php
@@ -0,0 +1,315 @@
+<?php
+
+// ********************* General *********************
+
+$lang['title'] = "POS - satış noktası";
+$lang['LnImage'] = "tr.png";
+$lang['Close'] = "Kapat";
+$lang['Submit'] = "Tamamla";
+$lang['Delete'] = "Sil";
+$lang['Edit'] = "Düzenle";
+$lang['Back'] = "Geri Dön";
+$lang['Areyousure'] = "Emin misiniz ?";
+$lang['Deletemessage'] = "Daha sonra bu veriyi kurtarmak mümkün olmayacaktır!";
+$lang['Deletedmessage'] = "veri silindi.";
+$lang['yesiam'] = "Evet, bunu silin!";
+$lang['Deleted'] = "Silindi!";
+$lang['Revenue'] = "Gelir";
+
+
+// ********************* Auth *********************
+
+$lang['Username'] = "Kullanıcı Adı";
+$lang['Password'] = "Parola";
+$lang['PasswordRepeat'] = "Şifreyi tekrar girin";
+$lang['Avatar'] = "Avatar";
+$lang['firstname'] = "İsim";
+$lang['lastname'] = "Soyadı";
+$lang['Role'] = "Rol";
+$lang['RoleAdimn'] = "Yönetici";
+$lang['RoleSales'] = "Satış ekibi";
+$lang['Email'] = "Email";
+$lang['lastActive'] = "Son Aktif Olunma";
+$lang['Login'] = "Oturum aç";
+$lang['Loginaccount'] = "Hesabınıza giriş yapın";
+$lang['login_incorrect'] = "Hatalı Giriş";
+$lang['LogOut'] = "Çıkış Yap";
+$lang['Adduser'] = "Kullanıcı Ekle";
+$lang['users'] = "Kullanıcılar";
+
+// ********************* Menu *********************
+
+$lang['POS'] = "POS";
+$lang['Product'] = "Ürün";
+$lang['Setting'] = "Ayarlar";
+$lang['Categories'] = "Kategoriler";
+$lang['Customers'] = "Müşteriler";
+$lang['Sales'] = "Satış";
+$lang['Reports'] = "Raporlar";
+$lang['Expense'] = "Harcamalar";
+$lang['People'] = "Kişiler";
+$lang['Suppliers'] = "Tedarikçiler";
+
+// ********************* product page *********************
+// ----infos
+$lang['ProductCode'] = "Ürün Kodu";
+$lang['ProductName'] = "Ürün Adı";
+$lang['Category'] = "Kategori";
+$lang['ProductTax'] = "vergi (%)";
+$lang['Cost'] = "Maliyet";
+$lang['Price'] = "Fiyat";
+$lang['Action'] = "Aksiyon";
+$lang['PurchasePrice'] = "Alış fiyatı";
+$lang['ProductDescription'] = "Ürün Açıklaması";
+$lang['Imageinput'] = "Girdi Görüntü";
+$lang['AddProduct'] = "Ürün ekle";
+$lang['ViewImage'] = "Görseli göster";
+$lang['ProductImage'] = "Resmi";
+$lang['ProductColor'] = "POS görüntü için renk seçin.";
+$lang['codeerror'] = "* Kod benzersiz olmalıdır";
+$lang['ImportProducts'] = "İthal Ürünler";
+$lang['DownloadCSV'] = "CSV İndir";
+$lang['UploadCSVfile'] = "CSV dosyası yükle";
+$lang['DownloadSample'] = "indir örnek";
+$lang['Standard'] = "Standart";
+$lang['Service'] = "Hizmet";
+$lang['Type'] = "Tip";
+$lang['AlertQt'] = "Uyarı Miktarı";
+$lang['ModifyStock'] = "Stok değiştirme";
+$lang['Stock'] = "Stok";
+$lang['Viewproduct'] = "ürün görüntüle";
+$lang['RowsNumber'] = "sütun sayısı";
+$lang['printBarcodes'] = "baskı barkod";
+$lang['combination'] = "kombinasyon";
+$lang['combinations'] = "kombinasyonlar";
+$lang['NoProduct'] = "eşleşme bulunamadı";
+$lang['Modify'] = "değiştirmek";
+$lang['All'] = "Herşey";
+$lang['Unit'] = "Ürün Birim";
+$lang['inclusive'] = "dahil";
+$lang['exclusive'] = "özel";
+$lang['TaxMethod'] = "vergi Yöntemi";
+$lang['ApplyFilter'] = "Filtreden geçir";
+$lang['ProductType'] = "Ürün tipi";
+$lang['ProductOptions'] = "virgülle ayırarak ürün Seçenekleri (ör: Daha Peynir, Cook Eh, vb ...)";
+
+
+// ********************* categories page *********************
+
+$lang['CategoryName'] = "Kategori adı";
+$lang['CreatedAt'] = "Oluşturulduğunda sahip 'de";
+$lang['AddCategory'] = "Kategori ekle";
+
+// ********************* expences page *********************
+
+$lang['Expences'] = "Harcamalar";
+$lang['Expence'] = "Harcamalar";
+$lang['ViewFile'] = "Dosyayı görüntüle";
+$lang['Reference'] = "Referans";
+$lang['Amount'] = "Fiyat";
+$lang['Note'] = "Not";
+$lang['Store'] = "Dükkan";
+$lang['AddExpence'] = "Harcama ekle";
+$lang['Attachment'] = "Dosya eki";
+$lang['AttachmentInfos'] = "Kabul edilen dosyalar (resim | pdf | doc | xls | zip) Maksimum boyutu: 2Mb";
+
+// ********************* Stores page *********************
+$lang['Stores'] = "Dükkanlar";
+$lang['AddStore'] = "Dükkan ekle";
+$lang['StoreName'] = "Dükkan adı";
+$lang['StorePhone'] = "Dükkan Telefon";
+$lang['CustomeFooter'] = "Alt Bilgiyi düzenle";
+$lang['manageTables'] = "Masaları yönet";
+$lang['TableName'] = "Masa adı";
+$lang['Addtable'] = "Masa ekle";
+$lang['AddZone'] = "Alan ekle";
+$lang['ZoneName'] = "Alan adı";
+$lang['Table'] = "Masa";
+$lang['ChooseZone'] = "Alan seçimi";
+$lang['StoreZone'] = "Dükkandaki Alanlar";
+$lang['StoreTables'] = "Dükkandaki Masalar";
+$lang['TablesDeleted'] = "Bu Alandaki Masa silinecek";
+
+// ********************* Setting page *********************
+
+$lang['Settings'] = "Ayarlar";
+$lang['Warehouses'] = "Depolar";
+$lang['SettingsDesciption'] = "Ayarlarınızı güncelleyin";
+$lang['Company'] = "Şirket Adı";
+$lang['CompanyLogo'] = "Şirket logosu";
+$lang['Phone'] = "Şirket Telefon";
+$lang['Currency'] = "Para Birimi Kodu";
+$lang['keyboardDisplay'] = "Sanal klavyeyi görüntüle";
+$lang['ReceiptFooter'] = "Makbuz altbilgi metin";
+$lang['ReceiptHeader'] = "Makbuz Üstbilgi Metin";
+$lang['themesPick'] = "Temanızı seçin";
+$lang['DefaultDiscount'] = "Standart İndirim";
+$lang['DefualtTax'] = "Varsayılan VERGİSİ";
+$lang['timezone'] = "Lütfen seçin saat dilimi";
+$lang['Country'] = "Ülke";
+$lang['City'] = "şehir";
+$lang['Adresse'] = "Adres";
+$lang['AddWarehouse'] = "Warehouse ekle";
+$lang['WarehouseName'] = "Depo Adı";
+$lang['WarehousePhone'] = "Depo Telefon";
+$lang['numberDecimal'] = "Ondalık sayı (Yuvarlama)";
+
+// ********************* Customer page *********************
+
+$lang['CustomerName'] = "Müşteri adı";
+$lang['CustomerPhone'] = "Telefon";
+$lang['CustomerEmail'] = "Email";
+$lang['CustomerDiscount'] = "İndirim";
+$lang['AddCustomer'] = "Müşteri ekle";
+
+// ********************* Waiter page *********************
+$lang['WaiterName'] = "garson Adı";
+$lang['withoutWaiter'] = "Garson olmadan";
+$lang['WaiterPhone'] = "Telefon";
+$lang['WaiterEmail'] = "Email";
+$lang['WaiterStore'] = "mağaza";
+$lang['AddWaiter'] = "Garson ekle";
+$lang['Waiters'] = "Garsonlar";
+$lang['Waiter'] = "Garson";
+
+// ********************* Supplier page *********************
+$lang['SupplierName'] = "Tedarikçi adı";
+$lang['SupplierPhone'] = "Telefon";
+$lang['SupplierEmail'] = "E-posta";
+$lang['AddSupplier'] = "Tedarikçi ekle";
+$lang['Supplier'] = "Tedarikçi";
+
+// ********************* register *********************
+
+$lang['Return'] = "Dönüş";
+$lang['RegisterDetails'] = "Kayıt Detayları";
+$lang['CloseRegister'] = "yakın&nbsp;kayıt";
+$lang['SwitchStore'] = "geçiş&nbsp;mağaza";
+$lang['open'] = "Açık";
+$lang['Stillopen'] = "Hala açık";
+$lang['close'] = "Kapat";
+$lang['closedat'] = "Kapanış saati";
+$lang['ChooseStore'] = "Bir mağaza seçin";
+$lang['PayementType'] = "payement Tipi";
+$lang['EXPECTED'] = "Beklenen";
+$lang['COUNTED'] = "sayılan";
+$lang['DIFFERENCES'] = "fark";
+$lang['PaymentsSummary'] = "Ödemeler Özeti";
+$lang['Openingtime'] = "Açılış zamanı";
+$lang['Openedby'] = "tarafından açılan";
+$lang['note'] = "Not";
+$lang['CashinHand'] = "Elinde nakit";
+$lang['CloseMessageRegister'] = "Daha sonra tutar kurtarmak mümkün olmayacaktır!";
+$lang['yesClose'] = "Evet, Kapatıldı.";
+$lang['TotalRevenue'] = "Toplam gelir";
+$lang['ClosedRegister'] = "Bu kayıt ile kapatıldı";
+$lang['at'] = "'da";
+$lang['Kitchenpage'] = "Mutfak&nbsp;sayfasına&nbsp;git";
+
+
+
+// ********************* POS page *********************
+
+$lang['AddNewCustomer'] = "Yeni Müşteri ekle";
+$lang['ChooseClient'] = "Müşteri seçin";
+$lang['WalkinCustomer'] = "Ayakta Müşteri";
+$lang['item'] = "ürün";
+$lang['TotalItems'] = "Toplam kalemler";
+$lang['Discount'] = "İndirim";
+$lang['tax'] = "Vergi";
+$lang['GrandTotal'] = "Genel Toplam";
+$lang['Paid'] = "ücretli";
+$lang['Change'] = "Değişiklik";
+$lang['Tel'] = "Tel:";
+$lang['ShowlastReceipt'] = "Son makbuzu göster";
+$lang['BarcodeScanner'] = "Barkod okuyucu";
+$lang['Product'] = "Ürün";
+$lang['price'] = "Fiyat";
+$lang['Quantity'] = "Miktar";
+$lang['SubTotal'] = "Ara Toplam";
+$lang['OrderTAX'] = "Vergi";
+$lang['CANCEL'] = "İPTAL";
+$lang['PAYEMENT'] = "ÖDEME";
+$lang['Search'] = "Arama";
+$lang['EmptyList'] = "Ürün Yok";
+$lang['SelectProduct'] = "Başlamak için Ürün seçiniz";
+$lang['INVOICE'] = "FATURA";
+$lang['invoice'] = "fatura";
+//---- Add Sale
+$lang['empty'] = "Boş";
+$lang['Receipt'] = "Makbuz";
+$lang['AddSale'] = "Satış ekle";
+$lang['Customer'] = "Müşteri";
+$lang['Total'] = "Genel Toplam";
+$lang['paymentMethod'] = "Ödeme şekli:";
+$lang['method'] = "yöntem:";
+$lang['Cash'] = "Nakit";
+$lang['Paid'] = "ücretli";
+$lang['ReturnChange'] = "Para üstü";
+$lang['CreditCard'] = "Kredi kartı";
+$lang['CreditCardNum'] = "Kredi Kartı Numarası (swipe)";
+$lang['CreditCardHold'] = "Kredi kartı sahibi";
+$lang['CODECV'] = "CVC Kodu";
+$lang['Year'] = "Yıl";
+$lang['Month'] = "Ay";
+$lang['Cheque'] = "Çek";
+$lang['ChequeNum'] = "Çek Numarası";
+$lang['SaleNum'] = "Satış Numarası";
+$lang['Date'] = "Tarih";
+$lang['DateReprise'] = "Tarih al";
+$lang['print'] = "Yazdır";
+$lang['saleStripesccess'] = "Satış başarıyla eklendi";
+$lang['Lowinventory'] = "düşük stok";
+$lang['requiresRegister'] = "Açık kayıt gerektirir";
+$lang['Options'] = "Seçenekler";
+$lang['NoTables'] = "Masa bulunmayan bir dükkan seçtiniz. Lütfen masa yaratın.";
+$lang['CancelAll'] = "İptal Et";
+
+// ********************* Sales page *********************
+
+$lang['paid'] = "ücretli";
+$lang['unpaid'] = "ödenmemiş";
+$lang['Partiallypaid'] = "kısmen ödenmiş";
+$lang['AddPayement'] = "Ödeme ekle";
+$lang['changeClient'] = "Değişim Müşteri";
+$lang['changeStatus'] = "Değişim Durumu";
+$lang['Createdby'] = "Tarafından Oluşturuldu";
+$lang['Number'] = "Numara";
+$lang['Status'] = "Durum";
+$lang['Payements'] = "Ödemeler";
+$lang['AddPayements'] = "Ödeme ekle";
+
+// ********************* Report page *********************
+
+$lang['Customers'] = "Müşteriler";
+$lang['in'] = "içinde";
+$lang['TodaySale'] = "Bugünün Satış";
+$lang['monthlyStats'] = "aylık İstatistikler";
+$lang['TopProducts'] = "Bu yılın en iyi 5 ürünü";
+$lang['ClientsStats'] = "Müşteriler İstatistikleri";
+$lang['SelectClient'] = "seçin Müşteri";
+$lang['ProductsStats'] = "Ürünler İstatistikleri";
+$lang['SelectProduct'] = "Ürün Seçiniz";
+$lang['SelectRange'] = "seç Aralığı";
+$lang['Product'] = "Ürün";
+$lang['GetReport'] = "Rapor alın";
+$lang['Profit'] = "Kâr";
+$lang['TotalProfit'] = "Toplam kâr";
+$lang['RegisterStats'] = "Kayıt İstatistikleri";
+$lang['SelectStore'] = "Mağaza Seç";
+
+//---- Add Sale
+$lang['January'] = "Ocak";
+$lang['February'] = "Şubat";
+$lang['March'] = "Mart";
+$lang['April'] = "Nisan";
+$lang['May'] = "Mayıs";
+$lang['June'] = "Haziran";
+$lang['July'] = "Temmuz";
+$lang['August'] = "Ağustos";
+$lang['September'] = "Eylül";
+$lang['October'] = "Ekim";
+$lang['November'] = "Kasım";
+$lang['December'] = "Aralık";
+
+ ?>
